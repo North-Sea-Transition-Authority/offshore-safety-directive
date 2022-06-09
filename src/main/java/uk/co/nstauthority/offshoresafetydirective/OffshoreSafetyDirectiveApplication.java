77a4a99ce08370e8156556730a1da419c1f4db1c
Daniel Ashworth <daniel.ashworth@fivium.co.uk>
2022-06-09 17:42:49 +0100

    Initial commit from spring initializer. No functionality

---
+++ src/main/java/uk/co/nstauthority/offshoresafetydirective/OffshoreSafetyDirectiveApplication.java
@@ -0,0 +1,13 @@
+package uk.co.nstauthority.offshoresafetydirective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffshoreSafetyDirectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffshoreSafetyDirectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/nstauthority/offshoresafetydirective/OffshoreSafetyDirectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.nstauthority.offshoresafetydirective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffshoreSafetyDirectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
