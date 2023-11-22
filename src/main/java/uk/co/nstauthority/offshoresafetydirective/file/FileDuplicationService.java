4b6c4ac79f597a28a549f6fa2927c06e60275364
cade-ayres <108355418+cade-ayres@users.noreply.github.com>
2023-11-22 16:49:26 +0000

    OSDOP-457/9 - copy-forward-documents (#546)

--- src/main/java/uk/co/nstauthority/offshoresafetydirective/file/FileDuplicationService.java
+++ src/main/java/uk/co/nstauthority/offshoresafetydirective/file/FileDuplicationService.java
@@ -1,95 +1,35 @@
 package uk.co.nstauthority.offshoresafetydirective.file;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uk.co.fivium.fileuploadlibrary.core.FileService;
 import uk.co.nstauthority.offshoresafetydirective.nomination.NominationDetail;
-import uk.co.nstauthority.offshoresafetydirective.nomination.NominationDetailDto;
-import uk.co.nstauthority.offshoresafetydirective.nomination.NominationDetailFileReference;
-import uk.co.nstauthority.offshoresafetydirective.nomination.NominationId;
-import uk.co.nstauthority.offshoresafetydirective.nomination.duplication.DuplicationUtil;
 
 @Service
 public class FileDuplicationService {
 
-  private final OldUploadedFileRepository uploadedFileRepository;
-  private final FileAssociationService fileAssociationService;
-  private final FileAssociationRepository fileAssociationRepository;
+  private final FileService fileService;
 
-  public FileDuplicationService(OldUploadedFileRepository uploadedFileRepository,
-                                FileAssociationService fileAssociationService,
-                                FileAssociationRepository fileAssociationRepository) {
-    this.uploadedFileRepository = uploadedFileRepository;
-    this.fileAssociationService = fileAssociationService;
-    this.fileAssociationRepository = fileAssociationRepository;
+  public FileDuplicationService(FileService fileService) {
+    this.fileService = fileService;
   }
 
   @Transactional
   public void duplicateFiles(NominationDetail nominationDetailToDuplicate,
                              NominationDetail nominationDetailToCascadeTo) {
-    var fileAssociations = fileAssociationService.getFileAssociationByFileAssociationReference(
-        new NominationDetailFileReference(nominationDetailToDuplicate));
 
-    var uploadFilesToDuplicate = fileAssociations.stream()
-        .map(FileAssociation::getUploadedFile)
-        .toList();
-
-    var duplicatedUploadedFiles = duplicateUploadedFiles(uploadFilesToDuplicate);
-    duplicateFileAssociations(nominationDetailToCascadeTo, fileAssociations, duplicatedUploadedFiles);
-  }
-
-  private List<OldUploadedFile> duplicateUploadedFiles(Collection<OldUploadedFile> uploadedFilesToDuplicate) {
-    var duplicatedFiles = new ArrayList<OldUploadedFile>();
-
-    uploadedFilesToDuplicate.forEach(file -> {
-      var newFile = BeanUtils.instantiateClass(OldUploadedFile.class);
-      DuplicationUtil.copyProperties(file, newFile, "id");
-      duplicatedFiles.add(newFile);
-    });
-    uploadedFileRepository.saveAll(duplicatedFiles);
-    return List.copyOf(duplicatedFiles);
-  }
-
-  private void duplicateFileAssociations(NominationDetail nominationDetailToCascadeTo,
-                                         Collection<FileAssociation> fileAssociations,
-                                         Collection<OldUploadedFile> duplicatedUploadedFiles) {
-    var fileAssociationToUploadedFileMap = fileAssociations.stream()
-        .collect(Collectors.toMap(
-            Function.identity(),
-            fileAssociation ->
-                getUploadedFileForFileAssociation(nominationDetailToCascadeTo, fileAssociation, duplicatedUploadedFiles)
+    fileService.findAll(
+        nominationDetailToDuplicate.getId().toString(),
+        FileUsageType.NOMINATION_DETAIL.getUsageType()
+    )
+        .forEach(uploadedFile -> fileService.copy(
+            uploadedFile,
+            builder -> builder
+                .withUsageId(nominationDetailToCascadeTo.getId().toString())
+                .withUsageType(uploadedFile.getUsageType())
+                .withDocumentType(uploadedFile.getDocumentType())
+                .build()
         ));
-
-    var duplicatedFileAssociations = new ArrayList<FileAssociation>();
-
-    fileAssociationToUploadedFileMap.forEach((fileAssociation, uploadedFile) -> {
-      var newAssociation = BeanUtils.instantiateClass(FileAssociation.class);
-      var detailDto = NominationDetailDto.fromNominationDetail(nominationDetailToCascadeTo);
-      DuplicationUtil.copyProperties(fileAssociation, newAssociation, "uuid");
-      newAssociation.setUploadedFile(uploadedFile);
-      newAssociation.setReferenceId(String.valueOf(detailDto.nominationDetailId().id()));
-      duplicatedFileAssociations.add(newAssociation);
-    });
-
-    fileAssociationRepository.saveAll(duplicatedFileAssociations);
-  }
-
-  private OldUploadedFile getUploadedFileForFileAssociation(NominationDetail nominationDetail,
-                                                            FileAssociation fileAssociation,
-                                                            Collection<OldUploadedFile> uploadedFiles) {
-    return uploadedFiles.stream().filter(uploadedFile ->
-            uploadedFile.getFileKey().equals(fileAssociation.getUploadedFile().getFileKey()))
-        .findFirst()
-        .orElseThrow(() -> new IllegalStateException(
-            "No file key [%s] found for uploaded files of nomination [%s]".formatted(
-                fileAssociation.getUploadedFile().getFileKey(),
-                new NominationId(nominationDetail).id()
-            )));
   }
 
 }
--- src/test/java/uk/co/nstauthority/offshoresafetydirective/file/FileDuplicationServiceTest.java
+++ src/test/java/uk/co/nstauthority/offshoresafetydirective/file/FileDuplicationServiceTest.java
@@ -1,34 +1,28 @@
 package uk.co.nstauthority.offshoresafetydirective.file;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Function;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.co.nstauthority.offshoresafetydirective.nomination.NominationDetailFileReference;
+import uk.co.fivium.fileuploadlibrary.core.FileService;
+import uk.co.fivium.fileuploadlibrary.core.FileUsage;
 import uk.co.nstauthority.offshoresafetydirective.nomination.NominationDetailTestUtil;
-import uk.co.nstauthority.offshoresafetydirective.util.assertion.PropertyObjectAssert;
 
 @ExtendWith(MockitoExtension.class)
 class FileDuplicationServiceTest {
 
   @Mock
-  private OldUploadedFileRepository uploadedFileRepository;
-
-  @Mock
-  private FileAssociationService fileAssociationService;
-
-  @Mock
-  private FileAssociationRepository fileAssociationRepository;
+  private FileService fileService;
 
   @InjectMocks
   private FileDuplicationService fileDuplicationService;
@@ -44,132 +38,51 @@ class FileDuplicationServiceTest {
         .withId(targetNominationDetailId)
         .build();
 
-    var uploadedFileA = UploadedFileTestUtil.builder()
-        .withId(UUID.randomUUID())
-        .withS3Key(UUID.randomUUID().toString())
-        .withDescription("file a")
-        .withBucketName("bucket/a")
-        .withFileContentType("typeA")
-        .withFileSizeBytes(100L)
-        .withVirtualFolder(VirtualFolder.NOMINATION_DECISION)
-        .withUploadedTimeStamp(Instant.now())
-        .withFilename("filename_a.txt")
-        .build();
-    var uploadedFileB = UploadedFileTestUtil.builder()
-        .withId(UUID.randomUUID())
-        .withS3Key(UUID.randomUUID().toString())
-        .withDescription("file b")
-        .withBucketName("bucket/b")
-        .withFileContentType("typeB")
-        .withFileSizeBytes(200L)
-        .withVirtualFolder(VirtualFolder.CASE_NOTES)
-        .withUploadedTimeStamp(Instant.now())
-        .withFilename("filename_b.txt")
-        .build();
-    var fileAssociationA = FileAssociationTestUtil.builder()
-        .withUploadedFile(uploadedFileA)
-        .withReferenceType(FileAssociationType.NOMINATION_DETAIL)
-        .withReferenceId(String.valueOf(sourceNominationDetailId))
-        .build();
-    var fileAssociationB = FileAssociationTestUtil.builder()
-        .withUploadedFile(uploadedFileB)
-        .withReferenceType(FileAssociationType.NOMINATION_DETAIL)
-        .withReferenceId(String.valueOf(sourceNominationDetailId))
-        .build();
+    var uploadedFileA = UploadedFileTestUtil.newBuilder().build();
+    var uploadedFileB = UploadedFileTestUtil.newBuilder().build();
 
-    when(fileAssociationService.getFileAssociationByFileAssociationReference(any(NominationDetailFileReference.class)))
-        .thenReturn(List.of(fileAssociationA, fileAssociationB));
+    when(fileService.findAll(
+        sourceNominationDetailId.toString(),
+        FileUsageType.NOMINATION_DETAIL.getUsageType()
+    ))
+        .thenReturn(List.of(uploadedFileA, uploadedFileB));
 
     fileDuplicationService.duplicateFiles(sourceNominationDetail, targetNominationDetail);
 
     @SuppressWarnings("unchecked")
-    ArgumentCaptor<List<OldUploadedFile>> uploadedFileCaptor = ArgumentCaptor.forClass(List.class);
-    verify(uploadedFileRepository).saveAll(uploadedFileCaptor.capture());
-
-    var duplicateUploadedFileA = uploadedFileCaptor.getValue()
-        .stream()
-        .filter(uploadedFile -> uploadedFile.getFileKey().equals(uploadedFileA.getFileKey()))
-        .findFirst()
-        .orElseThrow(() -> new IllegalStateException("File [A] was not duplicated with a matching file key"));
-
-    PropertyObjectAssert.thenAssertThat(duplicateUploadedFileA)
-        .hasFieldOrPropertyWithValue("fileKey", uploadedFileA.getFileKey())
-        .hasFieldOrPropertyWithValue("bucketName", uploadedFileA.getBucketName())
-        .hasFieldOrPropertyWithValue("virtualFolder", uploadedFileA.getVirtualFolder())
-        .hasFieldOrPropertyWithValue("filename", uploadedFileA.getFilename())
-        .hasFieldOrPropertyWithValue("fileContentType", uploadedFileA.getFileContentType())
-        .hasFieldOrPropertyWithValue("fileSizeBytes", uploadedFileA.getFileSizeBytes())
-        .hasFieldOrPropertyWithValue("uploadedTimeStamp", uploadedFileA.getUploadedTimeStamp())
-        .hasFieldOrPropertyWithValue("description", uploadedFileA.getDescription())
-        .hasAssertedAllPropertiesExcept("id");
-
-    assertThat(duplicateUploadedFileA)
-        .extracting(OldUploadedFile::getId)
-        .isNotEqualTo(uploadedFileA.getId());
-
-    var duplicateUploadedFileB = uploadedFileCaptor.getValue()
-        .stream()
-        .filter(uploadedFile -> uploadedFile.getFileKey().equals(uploadedFileB.getFileKey()))
-        .findFirst()
-        .orElseThrow(() -> new IllegalStateException("File [B] was not duplicated with a matching file key"));
-
-    PropertyObjectAssert.thenAssertThat(duplicateUploadedFileB)
-        .hasFieldOrPropertyWithValue("fileKey", uploadedFileB.getFileKey())
-        .hasFieldOrPropertyWithValue("bucketName", uploadedFileB.getBucketName())
-        .hasFieldOrPropertyWithValue("virtualFolder", uploadedFileB.getVirtualFolder())
-        .hasFieldOrPropertyWithValue("filename", uploadedFileB.getFilename())
-        .hasFieldOrPropertyWithValue("fileContentType", uploadedFileB.getFileContentType())
-        .hasFieldOrPropertyWithValue("fileSizeBytes", uploadedFileB.getFileSizeBytes())
-        .hasFieldOrPropertyWithValue("uploadedTimeStamp", uploadedFileB.getUploadedTimeStamp())
-        .hasFieldOrPropertyWithValue("description", uploadedFileB.getDescription())
-        .hasAssertedAllPropertiesExcept("id");
-
-    assertThat(duplicateUploadedFileB)
-        .extracting(OldUploadedFile::getId)
-        .isNotEqualTo(uploadedFileB.getId());
+    ArgumentCaptor<Function<FileUsage.Builder, FileUsage>> fileUsageFunctionCaptorForFileA =
+        ArgumentCaptor.forClass(Function.class);
 
     @SuppressWarnings("unchecked")
-    ArgumentCaptor<List<FileAssociation>> fileAssociationCaptor = ArgumentCaptor.forClass(List.class);
-    verify(fileAssociationRepository).saveAll(fileAssociationCaptor.capture());
+    ArgumentCaptor<Function<FileUsage.Builder, FileUsage>> fileUsageFunctionCaptorForFileB =
+        ArgumentCaptor.forClass(Function.class);
 
-    var duplicateFileAssociationA = fileAssociationCaptor.getValue()
-        .stream()
-        .filter(fileAssociation -> fileAssociation.getUploadedFile().equals(duplicateUploadedFileA))
-        .findFirst()
-        .orElseThrow(() ->
-            new IllegalStateException("FileAssociation [A] was not duplicated with link to duplicate file"));
+    verify(fileService).copy(eq(uploadedFileA), fileUsageFunctionCaptorForFileA.capture());
+    verify(fileService).copy(eq(uploadedFileB), fileUsageFunctionCaptorForFileB.capture());
 
-    PropertyObjectAssert.thenAssertThat(duplicateFileAssociationA)
-        .hasFieldOrPropertyWithValue("uploadedFile", duplicateUploadedFileA)
-        .hasFieldOrPropertyWithValue("fileStatus", fileAssociationA.getFileStatus())
-        .hasFieldOrPropertyWithValue("referenceType", fileAssociationA.getReferenceType())
-        .hasFieldOrPropertyWithValue("referenceId", String.valueOf(targetNominationDetailId))
-        .hasFieldOrPropertyWithValue("purpose", fileAssociationA.getPurpose())
-        .hasFieldOrPropertyWithValue("uploadedInstant", fileAssociationA.getUploadedInstant())
-        .hasAssertedAllPropertiesExcept("uuid");
+    assertThat(fileUsageFunctionCaptorForFileA.getValue().apply(FileUsage.newBuilder()))
+        .extracting(
+            FileUsage::usageId,
+            FileUsage::usageType,
+            FileUsage::documentType
+        )
+        .containsExactly(
+            targetNominationDetail.getId().toString(),
+            uploadedFileA.getUsageType(),
+            uploadedFileA.getDocumentType()
+        );
 
-    assertThat(duplicateFileAssociationA)
-        .extracting(FileAssociation::getUuid)
-        .isNotEqualTo(fileAssociationA.getUuid());
+    assertThat(fileUsageFunctionCaptorForFileB.getValue().apply(FileUsage.newBuilder()))
+        .extracting(
+            FileUsage::usageId,
+            FileUsage::usageType,
+            FileUsage::documentType
+        )
+        .containsExactly(
+            targetNominationDetail.getId().toString(),
+            uploadedFileB.getUsageType(),
+            uploadedFileB.getDocumentType()
+        );
 
-    var duplicateFileAssociationB = fileAssociationCaptor.getValue()
-        .stream()
-        .filter(fileAssociation -> fileAssociation.getUploadedFile().equals(duplicateUploadedFileB))
-        .findFirst()
-        .orElseThrow(() ->
-            new IllegalStateException("FileAssociation [B] was not duplicated with link to duplicate file"));
-
-    PropertyObjectAssert.thenAssertThat(duplicateFileAssociationB)
-        .hasFieldOrPropertyWithValue("uploadedFile", duplicateUploadedFileB)
-        .hasFieldOrPropertyWithValue("fileStatus", fileAssociationB.getFileStatus())
-        .hasFieldOrPropertyWithValue("referenceType", fileAssociationB.getReferenceType())
-        .hasFieldOrPropertyWithValue("referenceId", String.valueOf(targetNominationDetailId))
-        .hasFieldOrPropertyWithValue("purpose", fileAssociationB.getPurpose())
-        .hasFieldOrPropertyWithValue("uploadedInstant", fileAssociationB.getUploadedInstant())
-        .hasAssertedAllPropertiesExcept("uuid");
-
-    assertThat(duplicateFileAssociationB)
-        .extracting(FileAssociation::getUuid)
-        .isNotEqualTo(fileAssociationB.getUuid());
   }
 }
