fe279aa877b1166c50baa97995bb4eb771d7f741
Harry Devane <harry.devane@fivium.co.uk>
2025-05-29 13:28:28 +0100

    Update Spring Boot to v3.4.4 to fix vulnerabilities (#725)

--- src/main/java/uk/co/nstauthority/offshoresafetydirective/configuration/WebSecurityConfiguration.java
+++ src/main/java/uk/co/nstauthority/offshoresafetydirective/configuration/WebSecurityConfiguration.java
@@ -104,7 +104,7 @@ public class WebSecurityConfiguration {
 
     return RelyingPartyRegistration
         .withRegistrationId(samlProperties.getRegistrationId())
-        .assertingPartyDetails(party -> party
+        .assertingPartyMetadata(party -> party
             .entityId(samlProperties.getEntityId())
             .singleSignOnServiceLocation(samlProperties.getLoginUrl())
             .singleSignOnServiceBinding(Saml2MessageBinding.POST)
