1c3fdabf5e2b04687d557aad7dbe265caf809eaa
George O <115717712+GeorgeOG34@users.noreply.github.com>
2025-09-18 15:58:45 +0100

    EDU-7087: Add postgres session repo Bean to avoid Spring Session Attributes dupe key insert error (#732)

---
+++ src/main/java/uk/co/nstauthority/offshoresafetydirective/configuration/DatabaseConfiguration.java
@@ -0,0 +1,14 @@
+package uk.co.nstauthority.offshoresafetydirective.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.PostgreSqlJdbcIndexedSessionRepositoryCustomizer;
+
+@Configuration
+public class DatabaseConfiguration {
+
+  @Bean
+  public PostgreSqlJdbcIndexedSessionRepositoryCustomizer sessionRepositoryCustomizer() {
+    return new PostgreSqlJdbcIndexedSessionRepositoryCustomizer();
+  }
+}
